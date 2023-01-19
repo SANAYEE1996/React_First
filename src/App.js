9463f8b0ae0087c221c0c52e815d6b65fc989f8a
SANAYEE <dudtkd0219@gmail.com>
2023-01-19 23:51:29 +0900

    커머스 연결 테스트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [message, setMessage]=useState([]);
+  useEffect(()=>{
+    fetch("/item/all")
+        .then((res)=>{
+            console.log(res);
+          return res.json();
+        })
+        .then((data)=>{
+          console.log('asfdasdf',data);
+          setMessage(data);
+        });
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
